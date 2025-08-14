611ac5edc4150400133fc6b676ce983e1bd300cc
b1ackitty <jieunj0301@gmail.com>
2025-08-14 14:18:12 +0900

    [learn] UserCard 컴포넌트 작성
    - JSON 데이터를 읽어와 컴포넌트에 props로 전달
    - 커스텀 컴포넌트 리스트 렌더링
      - 고유한 key 값 설정
      - 일반적으로 데이터의 id 값 사용
    - 클래스 타입의 커스텀 컴포넌트 리스트 렌더링
      - React.Component 확장한 서브 클래스 컴포넌트 작성
      - render 메서드에서 JSX 반환

--- 07-custom-component/src/app.jsx
+++ 07-custom-component/src/app.jsx
@@ -1,7 +1,19 @@
+import users from './data/user.json'
+import { UserCard, UserCardClass } from './components/user-card'
+
 export default function App() {
   return (
     <section className="app demo">
       <h1>UserCard 커스텀 컴포넌트</h1>
+      {users.map((user) => (
+        <UserCardClass
+          key={user.id}
+          name={user.name}
+          phoneNumber={user.phoneNumber}
+          address={user.address}
+          age={user.age}
+        />
+      ))}
     </section>
   )
 }
