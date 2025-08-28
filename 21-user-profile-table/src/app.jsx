f9f9d5c7bb910a001b3d33efcb070560406b3798
b1ackitty <jieunj0301@gmail.com>
2025-08-28 10:50:25 +0900

    [learn] 사용자 프로필 테이블(접근성 준수)
    - 상태 관리
    - 부수 효과
    - 부수 효과 정리
    - 의미있는 구조화
    - 누구나 접근 가능한 표 설계

---
+++ 21-user-profile-table/src/app.jsx
@@ -0,0 +1,10 @@
+import { LearnSection } from '@/components'
+import UserProfileTable from './components/user-profile-table/user-profile-table'
+
+export default function App() {
+  return (
+    <LearnSection title="사용자 프로필 테이블">
+      <UserProfileTable />
+    </LearnSection>
+  )
+}
