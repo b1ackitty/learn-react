ee734a51d3bb4a731ec4db6d3c8066b2a81f604e
b1ackitty <jieunj0301@gmail.com>
2025-08-29 14:32:00 +0900

    [learn] 훅의 규칙 준수
    - 알아야 할 훅의 규칙 학습
    - ESLint 플러그인의 도움(개발자 경험 향상)

--- 25-hook-of-rules/src/app.jsx
+++ 25-hook-of-rules/src/app.jsx
@@ -1,10 +1,55 @@
+import { Component, useEffect, useState } from 'react'
 import { LearnSection } from '@/components'
 import RandomCountUp from '@/demo/app'
 
-export default function App() {
-  return (
-    <LearnSection title="리액트 훅의 규칙">
-      <RandomCountUp />
-    </LearnSection>
-  )
+export default class App extends Component {
+  render() {
+    // 1. 클래스 컴포넌트 안에서 훅 호출 ❌
+    // useEffect(() => {
+    //   console.log('mounted')
+    // }, [])
+
+    // callEffect()
+
+    return (
+      <LearnSection title="리액트 훅의 규칙">
+        <RandomCountUp />
+        <CustomComponent />
+      </LearnSection>
+    )
+  }
+}
+
+// 2. 일반 함수 안에서 훅 호출 ❌
+// function callEffect() {
+//   useEffect(() => {
+//     console.log('mounted')
+//   }, [])
+// }
+
+// 3. 사용자 정의 훅(use로 시작) 안에서 호출 ✅
+export function useCumtomHook() {
+  useEffect(() => {
+    console.log('마운트 이후 실행')
+  })
+}
+
+// 4. 함수형 컴포넌트(PascalCase) 훅 호출 ✅
+function CustomComponent() {
+  let isBlue = !true
+  // if (isBlue) return null
+
+  // 최대한 함수형 컴포넌트의 최상단에 훅을 위치시키는 것이 안전 ✅
+  const [isPending] = useState(true)
+  // for 문으로 감쌈 ❌
+  // if 문으로 감쌈 ❌
+  // if (isBlue) {
+  useEffect(() => {
+    if (isBlue) {
+      console.log('마운트 이후 실행')
+    }
+  }, [isBlue])
+  // }
+
+  return <p className="text-8xl">{isPending.toString()}</p>
 }
