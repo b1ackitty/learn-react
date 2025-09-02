311bc7ab9cfd0246e1579876456ada941e76cb90
b1ackitty <jieunj0301@gmail.com>
2025-09-02 14:47:24 +0900

    [learn] 개발 중, 초깃값이 변경될 때 바로 화면에 반응하도록 커스텀 훅 개선
    - 초기값 변경에 반응하도록 이펙트 훅 추가

--- 29-custom-hooks/src/app.tsx
+++ 29-custom-hooks/src/app.tsx
@@ -1,21 +1,17 @@
-import { ChangeEvent, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { LearnSection } from '@/components'
 import { useInput, useToggleState } from '@/hooks'
 import { tw } from '@/utils'
 
 export default function App() {
-  // [1] 토글 상태
-  // 테마(theme)
+  const inputProps = useInput<number>(0)
+
   const [darkTheme, toggleDarkTheme] = useToggleState(false)
+
+  // toggleDarkTheme 현재 렌더링에서와 다음 렌더링에서 동일한 함수이다. ✅ ❌
 
   const themeClassNames = darkTheme ? 'bg-slate-950 text-white' : ''
   const checkboxLabel = darkTheme ? '라이트 테마 전환' : '타크 테마 전환'
-
-  // [2] 인풋 상태
-  const [num, setNum] = useState<number>(0)
-  const handleChangeNum = (e: ChangeEvent<HTMLInputElement>) => {
-    setNum(Number(e.target.value))
-  }
 
   return (
     <LearnSection
@@ -38,27 +34,25 @@ export default function App() {
         <label htmlFor="number-input">숫자</label>
         <input
           type="number"
-          value={num}
-          onChange={handleChangeNum}
           id="number-input"
           className="my-2"
           min={0}
           max={10}
+          {...inputProps}
         />
       </div>
-      <output>{num}</output>
+      <output>{inputProps.value}</output>
       <CustomHookDemo />
     </LearnSection>
   )
 }
 
 function CustomHookDemo() {
-  const [toggle, setToggle] = useToggleState(true)
+  const inputProps = useInput<string>('로직 재사용')
 
+  const [toggle, setToggle] = useToggleState(true)
   const language = toggle ? 'ko' : 'en'
   const isKorean = language.includes('ko')
-
-  const inputProps = useInput('')
 
   return (
     <>
@@ -79,5 +73,35 @@ function CustomHookDemo() {
   )
 }
 
-// --------------------------------------------------------------------------
-// 커스텀 훅(use로 시작하는 사용자 정의 함수)
+function Parent() {
+  const [count, setCount] = useState(0)
+  const update = useCallback(
+    function () {
+      // setCount(count + 1)
+      setCount((c) => c + 1)
+    },
+    // [count]
+    []
+  )
+
+  // useEffect(() => {
+  //   update()
+  // }, [update])
+
+  return <Child count={count} update={update} />
+}
+
+interface ChildProps {
+  count: number
+  update: () => void
+}
+
+function Child({ count, update }: ChildProps) {
+  return (
+    <div className="p-12">
+      <button type="button" className="text-xl" onClick={update}>
+        {count}
+      </button>
+    </div>
+  )
+}
--- 29-custom-hooks/src/hooks/use-toggle.ts
+++ 29-custom-hooks/src/hooks/use-toggle.ts
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useToggleState(initialValue: boolean = true) {
-  // [관심사] 토글 상태 관리
-  // 상태
   const [toggle, setToggle] = useState<boolean>(initialValue)
 
-  // 상태 업데이트
   const update = () => setToggle((t) => !t)
 
-  // 사용자 정의 함수의 반환값 타입 [state, setState]
+  useEffect(() => {
+    setToggle(initialValue)
+  }, [initialValue])
+
   return [toggle, update] as const
 }
