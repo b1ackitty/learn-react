4b48eb41d30d2b7feea39e5a16760b5cc4ba8732
b1ackitty <jieunj0301@gmail.com>
2025-09-02 14:03:51 +0900

    [learn] 사용자 정의 훅 함수
    - useToggleState
    - useInput

--- 29-custom-hooks/src/app.tsx
+++ 29-custom-hooks/src/app.tsx
@@ -1,15 +1,12 @@
 import { ChangeEvent, useState } from 'react'
 import { LearnSection } from '@/components'
+import { useInput, useToggleState } from '@/hooks'
 import { tw } from '@/utils'
 
 export default function App() {
   // [1] 토글 상태
   // 테마(theme)
-  const [darkTheme, setDarkTheme] = useState<boolean>(false)
-  // 체크박스 기능: 테마 토글(checkbox)
-  const handleChecked = (e: ChangeEvent<HTMLInputElement>) => {
-    setDarkTheme(e.target.checked)
-  }
+  const [darkTheme, toggleDarkTheme] = useToggleState(false)
 
   const themeClassNames = darkTheme ? 'bg-slate-950 text-white' : ''
   const checkboxLabel = darkTheme ? '라이트 테마 전환' : '타크 테마 전환'
@@ -31,9 +28,11 @@ export default function App() {
           type="checkbox"
           id="theme-checkbox"
           checked={darkTheme}
-          onChange={handleChecked}
+          onChange={toggleDarkTheme}
         />
-        <label htmlFor="theme-checkbox">{checkboxLabel}</label>
+        <label htmlFor="theme-checkbox" className="select-none">
+          {checkboxLabel}
+        </label>
       </div>
       <div role="group" className="flex flex-col gap-2 my-4">
         <label htmlFor="number-input">숫자</label>
@@ -54,44 +53,31 @@ export default function App() {
 }
 
 function CustomHookDemo() {
-  // [관심사] 토글 상태 관리
-  // 상태
-  const [toggleState, setToggleState] = useState<boolean>(true)
-  // 상태 업데이트
-  const handleChangeLanguage = () => {
-    setToggleState((t) => !t)
-  }
+  const [toggle, setToggle] = useToggleState(true)
 
-  const language = toggleState ? 'ko' : 'en'
+  const language = toggle ? 'ko' : 'en'
   const isKorean = language.includes('ko')
 
-  // 인풋 상태 관리
-  const [name, setName] = useState('')
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-  }
+  const inputProps = useInput('')
 
   return (
     <>
       <div role="group" className="flex flex-col gap-2 my-4">
         <label htmlFor="user-input">이름</label>
-        <input
-          name={name}
-          onChange={handleChange}
-          type="text"
-          id="user-input"
-          className="my-2"
-        />
-        <output>{name || '이름 값 출력'}</output>
+        <input {...inputProps} type="text" id="user-input" className="my-2" />
+        <output>{inputProps.value || '이름 값 출력'}</output>
       </div>
       <button
         type="button"
-        className="cursor-pointer bg-black text-white p-2"
+        className="select-none cursor-pointer bg-black text-white p-2"
         lang={isKorean ? 'en' : 'ko'}
-        onClick={handleChangeLanguage}
+        onClick={setToggle}
       >
         {isKorean ? 'change english' : '한국어 전환'}
       </button>
     </>
   )
 }
+
+// --------------------------------------------------------------------------
+// 커스텀 훅(use로 시작하는 사용자 정의 함수)
---
+++ 29-custom-hooks/src/hooks/index.ts
@@ -0,0 +1,2 @@
+export { default as useToggleState } from './use-toggle'
+export { default as useInput } from './use-input'
