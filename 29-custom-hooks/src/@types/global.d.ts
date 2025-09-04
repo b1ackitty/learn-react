58df084e60c9e7bfa77a987feeb8d335e95eec64
b1ackitty <jieunj0301@gmail.com>
2025-09-04 10:25:51 +0900

    [learn] useMutation 훅 작성
    - 서버에 데이터 뮤테이션 요청
    - POST 요청 처리

--- 29-custom-hooks/src/@types/global.d.ts
+++ 29-custom-hooks/src/@types/global.d.ts
@@ -1,9 +1,9 @@
-type Timeout = ReturnType<typeof setTimeout> | number | undefined
+export type Timeout = ReturnType<typeof setTimeout> | number | undefined
 
 export type Status = 'idle' | 'pending' | 'resolved' | 'rejected'
 
-export interface State<Type> {
+export interface State<DataType, ErrorType = Error> {
   status: Status
-  error: null | Error
-  data: null | Type
+  error: null | ErrorType
+  data: null | DataType
 }
