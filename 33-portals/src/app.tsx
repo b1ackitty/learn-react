e40ea6be9e0a2ec8107325af8195d246157614e2
b1ackitty <jieunj0301@gmail.com>
2025-09-08 10:36:13 +0900

    [learn] 포털(Portals) 개념 정리

--- 33-portals/src/app.tsx
+++ 33-portals/src/app.tsx
@@ -1,10 +1,14 @@
-import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo'
+import { Info } from 'lucide-react'
+import { AlertPortal, LearnSection } from '@/components'
 
 export default function App() {
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection title="리액트의 포털(차원 이동)" showTitle className="p-10">
+      <AlertPortal>
+        <p className="flex gap-1">
+          <Info /> 포털을 사용해 컴포넌트 렌더링
+        </p>
+      </AlertPortal>
     </LearnSection>
   )
 }
