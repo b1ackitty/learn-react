8e200c75345ef8d1c35b7b1642418d2c524482b0
b1ackitty <jieunj0301@gmail.com>
2025-09-16 16:38:29 +0900

    [learn] use 함수(API) 4
    - 에러 핸들링(오류 처리)
    - [추천] ErrorBoundary
    - Prmoise.catch() 메서드 사용 (컴포넌트 내부에서 오류 처리)

--- 44-deferred-value-transition/src/features/use-api/components/users.tsx
+++ 44-deferred-value-transition/src/features/use-api/components/users.tsx
@@ -23,7 +23,7 @@ export default function Users() {
   // Suspense 경계 내에서만 사용할 수 있습니다.
 
   // const [loading, setLoading] = useState<boolean>(false)
-  // const [error, setError] = useState(null)
+  // const [error, setError] = useState<null | Error>(null)
   // const [data, setData] = useState<null | User[]>(null)
 
   // useEffect(() => {
@@ -41,8 +41,15 @@ export default function Users() {
   //     })
   // }, [])
 
-  // [안전] Promise 인스턴스를 기억하고 있으므로 렌더링과 상관 없이 동일한 참조를 하위 컴포넌트에 전달
-  const usersPromise = useMemo(() => fetchUsers(), [])
+  // [안전] Promise 인스턴스를 기억하고 있으므로 렌더링과 상관없이 동일한 참조를 하위 컴포넌트에 전달
+  const usersPromise = useMemo(
+    () =>
+      fetchUsers().catch((error) => {
+        // 오류 처리
+        setError(error)
+      }),
+    []
+  )
 
   // [위험] 컴포넌트가 리렌더링 되면 무한 반복에 빠질 수 있다.
   const [message, setMessage] = useState<string>('')
@@ -56,15 +63,35 @@ export default function Users() {
 
   // const usersPromise = fetchUsers() // ... Promise 2
 
+  // 오류 처리 [2]
+  // Promise 거절될 경우,
+  // catch() 메서드를 사용해 컴포넌트 내부에서 오류 처리
+  const [error, setError] = useState<null | Error>(null)
+
+  if (error) {
+    return (
+      <div
+        role="alert"
+        className={tw`
+          text-red-600 bg-red-50 rounded
+          p-2 border-2 border-red-700
+        `}
+      >
+        {error.message}
+      </div>
+    )
+  }
+
   return (
     <section className="mt-6 p-6 max-w-xl bg-gray-50 rounded-xl shadow-lg">
       <h3 className="text-xl font-bold mb-6 text-center text-indigo-700">
-        사용자 목록({message})
+        사용자 목록 ({message})
       </h3>
 
       {/* {loading && <Loading />} */}
       {/* {!loading && data && <UserList items={data} />} */}
 
+      {/* 오류 처리 [1] */}
       <ErrorBoundary>
         <Suspense fallback={<Loading />}>
           <UserList fetchUsersPromise={usersPromise} />
@@ -78,11 +105,11 @@ export default function Users() {
 function UserList({
   fetchUsersPromise,
 }: {
-  fetchUsersPromise: Promise<User[]>
+  fetchUsersPromise: Promise<User[] | void>
 }) {
   // use 함수는 Promise 인스턴스를 전달 받아
   // 결과가 해결(resolved)된 값을 반환합니다.
-  // 오류가 발생하면, ErrorBoundary
+  // 오류가 발생한 경우, ErrorBoundary
   // 컴포넌트에서 에러 처리합니다.
 
   const items = use(fetchUsersPromise)
@@ -93,7 +120,7 @@ function UserList({
   // const items = use(fetchUsers())
 
   // use 함수는 훅 함수가 아니므로,
-  // if문 또는 for문 내부에서 사용할 수 있다!
+  // if문 또는 for문 내부에서 사용이 가능!
   // let items: User[] = []
   // if (fetchUsersPromise) {
   //   items = use(fetchUsersPromise)
@@ -101,7 +128,7 @@ function UserList({
 
   return (
     <ul className="space-y-2">
-      {items.map((user) => (
+      {items?.map((user) => (
         <li
           key={user.id}
           className={tw(
@@ -139,7 +166,7 @@ async function fetchUsers(): Promise<User[]> {
   await wait(1.2)
 
   // 오류 상황 체크
-  // throw new Error('데이터 가져오기에 실패했습니다.')
+  throw new Error('데이터 가져오기에 실패했습니다.')
 
   console.log('fetchUsers')
 
