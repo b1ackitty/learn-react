47924f986bfbcdc0dff55f6a131f9863ae50090f
b1ackitty <jieunj0301@gmail.com>
2025-09-22 10:40:23 +0900

    [learn] Next.js의 소프트 내비게이션
    - <Link> 컴포넌트
    - 레이아웃(서버 컴포넌트) 리렌더링 ❌

--- learn-next-js/03-file-based-routing/src/app/page.tsx
+++ learn-next-js/03-file-based-routing/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Section } from '@/components'
 
 export default function HomePage() {
   return (
-    <Section title="홈 페이지">
+    <Section title="홈 페이지" className="bg-amber-300">
       <p>홈 페이지 방문</p>
     </Section>
   )
