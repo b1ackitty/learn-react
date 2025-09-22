54784b23b69d19ae75fccf1b4db63f4a9f265b03
b1ackitty <jieunj0301@gmail.com>
2025-09-22 17:29:07 +0900

    [learn] 카카오 도서 API 요청/응답
    - 카카오 디벨로프 가입/로그인 후 REST API 키 발급 활용
    - 응답 타입 정의 활용

--- learn-next-js/05-loading-error/src/app/books/api.ts
+++ learn-next-js/05-loading-error/src/app/books/api.ts
@@ -1,21 +1,34 @@
-// 리액트의 use 함수 활용
-// const { quotes } = use<QuoteResponse>(
-//   fetch('https://dummyjson.com/quotes').then((response) => response.json())
-// )
-
-// --------------------------------------------------------------------------
-
 import { wait } from '@/utils'
-import type { QuoteResponse } from './types'
+import type { KakaoBooksResponse } from './types'
 
-// 데이터 가져오기 비동기 함수
-export const fetchQuotes = async (): Promise<QuoteResponse['quotes']> => {
-  // 서버 컴포넌트의 데이터 가져오기 (✅ 권장!!! 정적 렌더링 1회)
-  const response = await fetch('https://dummyjson.com/quotes')
-  const { quotes } = await response.json()
+console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY)
 
-  await wait(1.1)
-  // throw new Error('도서 목록 데이터 가져오기에 실패했습니다.')
+const headersList = {
+  'Content-Type': 'Application/json',
+  Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+}
 
-  return quotes
+interface Options {
+  query: string
+}
+
+export const fetchKakaoBooks = async ({
+  query,
+}: Options): Promise<KakaoBooksResponse> => {
+  const encodedQuery = encodeURIComponent(query)
+
+  await wait()
+
+  const response = await fetch(
+    `https://dapi.kakao.com/v3/search/book?query=${encodedQuery}`,
+    {
+      headers: headersList,
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error('카카오 도서 API에서 데이터 가져오기에 실패했습니다.')
+  }
+
+  return response.json()
 }
